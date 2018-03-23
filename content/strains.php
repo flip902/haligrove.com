24e66429a859eef03315270de16394fd991f024a
flip902 <flip902@gator4096.hostgator.com>
2018-03-23 00:12:31 -0500

    sold out

--- content/strains.php
+++ content/strains.php
@@ -1,5 +1,12 @@
+<?php
+// Date in the past
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
+?>
+
 <?php include("header.php");?>
-<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+
 
   <div class="bodyContainer">
     
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+// Date in the past
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
+?>
+
+
 <?php include("includes/header.php");?>
   <section class="indexHeight">
     <!-- Welcome Header -->
